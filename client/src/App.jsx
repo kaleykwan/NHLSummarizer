2da9b7283d533f977334143c709437d7bc955f3e
Kaley Kwan <kaleymkwan@gmail.com>
2024-11-25 17:03:43 -0800

    add vercel speed insights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import { GameList } from "./components/GameList";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { inject } from '@vercel/analytics';
- 
+import { inject } from "@vercel/analytics";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 inject();
 
 const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GameList />
+      <SpeedInsights />
     </QueryClientProvider>
   );
 }
