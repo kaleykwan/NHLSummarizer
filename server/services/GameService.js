f2c729b2257c03755eae28021490c03ed0b4ca30
Kaley Kwan <kaleymkwan@gmail.com>
2024-12-10 19:13:15 -0500

    remove delete many from game service

--- server/services/GameService.js
+++ server/services/GameService.js
@@ -15,7 +15,6 @@ export default async function getSchedule() {
   const gameWeek = schedule.gameWeek;
 
   let collection = db.collection("games");
-  collection.deleteMany({});
   for (let day of gameWeek) {
     for (let game of day.games) {
       try {
